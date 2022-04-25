80ec6b2f23c171827bf046b391187d2cc38c3099
saharshshukla86@gmail.com <saharshshukla86@gmail.com>
2022-04-26 00:53:33 +0530

    kunalkushwaAssignment update

--- src/assignment/flow_Of_Program/Qst1.java
+++ src/assignment/flow_Of_Program/Qst1.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class Qst1 {
     public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
         System.out.println("Enter any year: ");
-        int yr=scn.nextInt();
+        int yr = scn.nextInt();
         if(yr % 4 == 0){
             System.out.println("Entered year is leap year");
         }else{
